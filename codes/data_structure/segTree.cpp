179015609a7330c3ad7e6348217161d2900bc412
admin <admin@admins-MacBook-Pro.local>
2021-09-18 12:44:07 -0400

    added explanation on segTree.cpp

--- codes/data_structure/segTree.cpp
+++ codes/data_structure/segTree.cpp
@@ -1,3 +1,4 @@
+//dont forget that is 0 indexed
 const int N = 1e5;  // limit for array size
 int n;  // array size
 int t[2 * N];
@@ -10,7 +11,7 @@ void modify(int p, int value) {  // set value at position p
   for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
 }
 
-int query(int l, int r) {  // sum on interval [l, r)
+int query(int l, int r) { //[l, r) does not include r in range 
   int res = 0;
   for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
     if (l&1) res += t[l++];
@@ -20,3 +21,4 @@ int query(int l, int r) {  // sum on interval [l, r)
 }
 
 //read like for (int i = 0; i < n; ++i) scanf("%d", t + n + i);
+//code from https://codeforces.com/blog/entry/18051
