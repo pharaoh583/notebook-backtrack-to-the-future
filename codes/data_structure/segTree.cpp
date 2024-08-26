8459aae39f0ed5cae4a3471c31fdd9b0c862c8ed
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2024-08-26 09:58:46 -0400

    Use OOP for seg tree

--- codes/data_structure/segTree.cpp
+++ codes/data_structure/segTree.cpp
@@ -1,24 +1,42 @@
-//dont forget that is 0 indexed
-const int N = 1e5;  // limit for array size
-int n;  // array size
-int t[2 * N];
-
-void build() {  // build the tree
-  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
-}
-
-void modify(int p, int value) {  // set value at position p
-  for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
-}
-
-int query(int l, int r) { //[l, r) does not include r in range 
-  int res = 0;
-  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-    if (l&1) res += t[l++];
-    if (r&1) res += t[--r];
-  }
-  return res;
-}
-
-//read like for (int i = 0; i < n; ++i) scanf("%d", t + n + i);
+class SegTree {
+private:
+    int n;
+    vi A, t;
+    
+    void build() {
+        forn(i, n) {
+            t[i + n] = A[i];
+        }
+        
+        for(int i = n - 1; i > 0; i--) {
+            t[i] = t[i<<1] + t[i<<1|1];
+        }
+    }
+    
+public:
+    SegTree(int sz) : n(sz), t(2 * sz) {}
+    SegTree(const vi &initA) : SegTree((int)initA.size()) {
+        A = initA;
+        build();
+    }
+    
+    ll RSQ(int l, int r) {
+        ll tot = 0;
+        for(l += n, r += n; l < r; l>>=1, r>>=1) {
+            if(l&1) tot += t[l++];
+            if(r&1) tot += t[--r];
+        }
+        return tot;
+    }
+    
+    void update(int pos, int val) {
+        for(t[pos += n] = val; pos > 1; pos >>=1) t[pos>>1] = t[pos] + t[pos^1];
+    }
+    
+    void print() {
+        forn(i,2 * n) {
+            cout<<t[i]<<" ";
+        } cout<<el;
+    }
+};
 //code from https://codeforces.com/blog/entry/18051
