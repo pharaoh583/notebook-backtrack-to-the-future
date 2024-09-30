23ba3c10f2dc7357ed2cfaf95de771b316502b46
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2024-09-29 22:54:30 -0400

    add mod inverse function

--- codes/math/fact_inv.cpp
+++ codes/math/fact_inv.cpp
@@ -8,3 +8,24 @@ void init(){
 	INV[1] = 1; forsn(i, 2, maxi) INV[i] = M - (ll)(M/i)*INV[M%i]%M;
 	FI[0] = 1; forsn(i, 1, maxi) FI[i] = FI[i-1]*INV[i] %M;
 }
+
+// works if gcd(A, M) = 1
+int modInverse(int A, int M){
+    int m0 = M;
+    int y = 0, x = 1;
+
+    if (M == 1) return 0;
+
+    while (A > 1) {
+        int q = A / M;
+        int t = M;
+        M = A % M, A = t;
+        t = y;
+        y = x - q * y;
+        x = t;
+    }
+
+    if (x < 0) x += m0;
+
+    return x;
+}
