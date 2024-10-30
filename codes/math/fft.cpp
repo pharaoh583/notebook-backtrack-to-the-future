3a1bbfb945654ec10fdf3bf4e7817c60d4fc534a
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2024-10-30 10:39:23 -0400

    review fft code

--- codes/math/fft.cpp
+++ codes/math/fft.cpp
@@ -1,4 +1,6 @@
 // MAXN must be power of 2 !!
+// MAXN = 131072, or MAXN = 1048576 
+// remember that poly(n) * poly(m) = poly(n * m) to define MAXN
 // MOD-1 needs to be a multiple of MAXN !!
 // big mod and primitive root for NTT:
 typedef ll tf;
@@ -35,7 +37,7 @@ CD root(int n, bool inv){
 CD cp1[MAXN+9],cp2[MAXN+9];
 int R[MAXN+9];
 void dft(CD* a, int n, bool inv){
-	forsn(i,0,n)if(R[i]<i)swap(a[R[i]],a[i]);
+	forn(i,n)if(R[i]<i)swap(a[R[i]],a[i]);
 	for(int m=2;m<=n;m*=2){
 		double z=2*pi/m*(inv?-1:1); // FFT
 		CD wi=CD(cos(z),sin(z)); // FFT
@@ -47,7 +49,7 @@ void dft(CD* a, int n, bool inv){
 			}
 		}
 	}
-	if(inv)forsn(i,0,n)a[i]/=n; // FFT
+	if(inv)forn(i,n)a[i]/=n; // FFT
 	//if(inv){ // NTT
 	//	CD z(pm(n,MOD-2)); // pm: modular exponentiation
 	//	forsn(i,0,n)a[i]=a[i]*z;
@@ -57,16 +59,16 @@ poly multiply(poly& p1, poly& p2){
 	int n=p1.size()+p2.size()+1;
 	int m=1,cnt=0;
 	while(m<=n)m+=m,cnt++;
-	forsn(i,0,m){R[i]=0;forsn(j,0,cnt)R[i]=(R[i]<<1)|((i>>j)&1);}
-	forsn(i,0,m)cp1[i]=0,cp2[i]=0;
-	forsn(i,0,p1.size())cp1[i]=p1[i];
-	forsn(i,0,p2.size())cp2[i]=p2[i];
+	forn(i,m){R[i]=0;forn(j,cnt)R[i]=(R[i]<<1)|((i>>j)&1);}
+	forn(i,m)cp1[i]=0,cp2[i]=0;
+	forn(i,p1.size())cp1[i]=p1[i];
+	forn(i,p2.size())cp2[i]=p2[i];
 	dft(cp1,m,false);dft(cp2,m,false);
-	forsn(i,0,m)cp1[i]=cp1[i]*cp2[i];
+	forn(i,m)cp1[i]=cp1[i]*cp2[i];
 	dft(cp1,m,true);
 	poly res;
 	n-=2;
-	forsn(i,0,n)res.pb((tf)floor(cp1[i].real()+0.5)); // FFT
+	forn(i,n)res.pb((tf)floor(cp1[i].real()+0.5)); // FFT
 	//fore(i,0,n)res.pb(cp1[i].x); // NTT
 	return res;
 }
