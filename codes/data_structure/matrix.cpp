65e5082a6fbeb06e9f224ec33f2e4e7dc0c56437
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2024-10-30 11:23:20 -0400

    use class for matrix struct

--- codes/data_structure/matrix.cpp
+++ codes/data_structure/matrix.cpp
@@ -1,36 +1,24 @@
-#define forn(i,n) for(int i = 0; i < int(n); i++)
-const int mod = 1e9 + 7;
- 
-struct M{
-	ll m[2][2];
-	M(){
-		m[0][0] = 0;
-		m[0][1] = 1;
-		m[1][0] = -1;
-		m[1][1] = 1;
-	}
-	M(int a){
-		m[0][0] = a;
-		m[0][1] = a;
-		m[1][0] = a;
-		m[1][1] = a;
-	}
-	M operator * (M b){
-		M aux = M(0);
-		forn(i, 2){
-			forn(j, 2){
-				forn(k, 2){
-					aux.m[i][j] += m[i][k] * b.m[k][j];
-				}
-			}
-		}
-		return aux;
-	}
-};
- 
-M exp(M a, int n){
-	if(n == 1) return a;
-	M z = exp(a, n/2);
-	if(n%2 == 0) return z * z;
-	return z * z * a;
-}
+class Matrix{
+public:
+    int r, c;
+    vector<vi> m;
+    Matrix(int rows, int cols) : r(rows), c(cols){
+        m = vector<vi>(r, vi(c, 0));
+    }
+    Matrix(int n) : Matrix(n, n){
+        forn(i, r) m[i][i] = 1; 
+    }
+    Matrix(vector<vi> mt) : Matrix(mt.size(), mt[0].size()) {
+        m = mt;
+    }
+    Matrix operator * (Matrix b) {
+        Matrix res(r, b.c);
+        forn(i, r) {
+            forn(j, b.c) {
+                forn(k, c) {
+                    res.m[i][j] = (res.m[i][j] + (m[i][k] * b.m[k][j])%M)%M;
+                }
+            }
+        }
+        return res;
+    }
