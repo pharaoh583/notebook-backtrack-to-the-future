bfd6e615c1731b29a8d483c64ce07c6961ae96a0
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2026-04-22 14:44:37 -0400

    feat: final version of prefix automaton

--- codes/strings/kmp.cpp
+++ codes/strings/kmp.cpp
@@ -10,15 +10,14 @@ vi prefix_function(string s) {
     return pi;
 }
 
-
-int wh[MAXN+2][MAXC]; //wh[i][j] = a donde vuelvo si estoy en i y pongo una j 
+int wh[maxn+2][maxc]; //wh[i][j] = a donde vuelvo si estoy en i y pongo una j 
 void prefix_automaton(string &s){ 
     int lps=0; 
-    forn(j,MAXC) wh[0][j]=0; 
+    forn(j,maxc) wh[0][j]=0; 
     wh[0][s[0]-'A'] = 1; 
-    forsn(i,1,SZ(s)+1){ 
-        forn(j,MAXC) wh[i][j]=wh[lps][j]; 
-        if(i<SZ(s)){ 
+    forsn(i,1,s.size()+1){ 
+        forn(j,maxc) wh[i][j]=wh[lps][j]; 
+        if(i<s.size()){ 
             wh[i][s[i]-'A'] = i+1; 
             lps = wh[lps][s[i]-'A']; 
         } 
