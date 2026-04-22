a4e3928962ecdde1d945ae345e1c22cd38a43fe3
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2026-04-22 14:39:07 -0400

    feat: change build prefix automaton algorithm

--- codes/strings/kmp.cpp
+++ codes/strings/kmp.cpp
@@ -9,17 +9,18 @@ vi prefix_function(string s) {
     }
     return pi;
 }
-void compute_automaton(string s, vector<vi>& aut) {
-    s += '#';
-    int n = s.size();
-    vi pi = prefix_function(s);
-    aut.assign(n, vi(26));
-    forn(i, n) {
-        forn(c, 26) {
-            if (i > 0 && 'a' + c != s[i])
-                aut[i][c] = aut[pi[i-1]][c];
-            else
-                aut[i][c] = i + ('a' + c == s[i]);
-        }
+
+
+int wh[MAXN+2][MAXC]; //wh[i][j] = a donde vuelvo si estoy en i y pongo una j 
+void prefix_automaton(string &s){ 
+    int lps=0; 
+    forn(j,MAXC) wh[0][j]=0; 
+    wh[0][s[0]-'A'] = 1; 
+    forsn(i,1,SZ(s)+1){ 
+        forn(j,MAXC) wh[i][j]=wh[lps][j]; 
+        if(i<SZ(s)){ 
+            wh[i][s[i]-'A'] = i+1; 
+            lps = wh[lps][s[i]-'A']; 
+        } 
     }
 }
