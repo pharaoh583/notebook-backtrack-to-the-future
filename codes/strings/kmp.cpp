885f4e4adb6f440b331a0937bb01a65252b1597a
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2026-04-22 13:42:43 -0400

    feat: add kmp and prefix automaton creation
    Refactor KMP algorithm implementation with improved variable names and structure.

---
+++ codes/strings/kmp.cpp
@@ -0,0 +1,25 @@
+vector<int> prefix_function(string s) {
+    int n = s.size();
+    vector<int> pi(n);
+    forsn(i, 1, n) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j]) j = pi[j-1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+void compute_automaton(string s, vector<vector<int>>& aut) {
+    s += '#';
+    int n = s.size();
+    vi pi = prefix_function(s);
+    aut.assign(n, vi(26));
+    forn(i, n) {
+        forn(j, 26) {
+            if (i > 0 && 'a' + c != s[i])
+                aut[i][c] = aut[pi[i-1]][c];
+            else
+                aut[i][c] = i + ('a' + c == s[i]);
+        }
+    }
+}
